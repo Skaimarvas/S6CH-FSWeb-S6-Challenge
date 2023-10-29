05f7428482e1e99afbe6f78e51cf1038a36ed835
skaimarvas <skaimarvas@gmail.com>
2023-10-30 01:10:30 +0300

    Menu boyutu tekrar hesaplandı

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import React, { useEffect, useState } from "react";
 import Karakter from "./components/Karakter";
 import Header from "./components/Header";
 import styled from "styled-components";
-import Ozellik from "./components/Ozellik";
+
+const Menu = styled.div`
+  display: border-box;
+  border: 1px solid lime;
+  margin: 0 1rem;
+
+  visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -14,41 +21,18 @@ const App = () => {
   // sync up with, if any.
 
   const [characters, setCharacters] = useState(null);
+  const [menuVisible, SetMenuvisible] = useState(false);
 
-  const Row = styled.div`
-    display: flex;
-    flex-direction: row;
-  `;
-  const Col = styled.div`
-    display: flex;
-    flex: 0.22;
-    flex-direction: column;
-    color: black;
-    background-color: grey;
-    opacity: 0.4;
-    padding: 1rem;
-    margin: 1rem;
-    border-radius: 4px;
-    visibility: hidden;
-  `;
-  const Col2 = styled.div`
-    display: flex;
-    flex: 0.78;
-    flex-direction: column;
-    color: black;
-    background-color: grey;
-    opacity: 0.4;
-    padding: 1rem;
-    margin: 1rem;
-    border-radius: 1rem;
-  `;
+  const toggleVisibility = () => {
+    SetMenuvisible(!menuVisible);
+  };
 
   useEffect(() => {
     axios
       .get("https://swapi.dev/api/people")
       .then((res) => {
-        setCharacters(res.data);
-        console.log("Data", res.data.results);
+        setCharacters(res.data.results);
+        console.log("Data", res.data);
       })
       .catch(() => {
         console.log("error");
@@ -57,22 +41,17 @@ const App = () => {
 
   return (
     <div className="App">
-      <Header />
+      <Header toggleVisibility={toggleVisibility} />
 
-      <Row>
-        <Col>
-          {/* {characters ? (
-            characters.map((char) => (
-              <Karakter character={char} key={char.name} />
-            ))
-          ) : (
-            <h3>Yükleniyor</h3>
-          )} */}
-        </Col>
-        <Col2>
-          {/* <Ozellik ozellik={characters ? characters[0] : null} /> */}
-        </Col2>
-      </Row>
+      <Menu visible={menuVisible}>
+        {characters ? (
+          characters.map((char) => (
+            <Karakter character={char} key={char.name} />
+          ))
+        ) : (
+          <h3>Yükleniyor</h3>
+        )}
+      </Menu>
     </div>
   );
 };
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,29 +1,80 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
+
+const Row = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+const Col1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 0.22;
+  background-color: grey;
+`;
+const Col = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: black;
+  width: 100%;
+  margin-left: 0;
+  border-radius: 4px;
+`;
+const Col2 = styled.div`
+  display: flex;
+  flex: 0.77;
+  color: black;
+  background-color: grey;
+  opacity: ${(props) => (props.visible ? 1 : 0)};
+  padding: 1rem;
+  margin: 1rem;
+  height: 70vh;
+  border-radius: 1rem;
+  display: ${(props) => (props.visible ? "block" : "none")};
+`;
+const Button1 = styled.button`
+  border-radius: 1rem;
+  cursor: pointer;
+  background-color: #007bff;
+  color: white;
+  &:hover {
+    background-color: #e0e0e0;
+    color: black;
+  }
+`;
 
 const Karakter = ({ character }) => {
-  console.log("Karakter", character.name);
-  const SearchInput = styled.input`
-    padding: 0.5rem;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    width: 250px;
-    font-size: 16px;
-  `;
-  const SearchButton = styled.button`
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 4px;
-    padding: 0.5rem 1rem;
-    margin-left: 10px;
-    cursor: pointer;
-  `;
-
+  const [menuVisible, SetMenuvisible] = useState(false);
+  const toggleVisibility = () => {
+    SetMenuvisible(!menuVisible);
+  };
   return (
     <div>
-      <h2>{character.name}</h2>
-      {console.log("Karakter", character.name)}
+      <Row>
+        <Col1>
+          <Col>
+            <Button1 onClick={toggleVisibility}>
+              {" "}
+              <h2>{character.name}</h2>{" "}
+            </Button1>
+          </Col>
+        </Col1>
+        <Col2 visible={menuVisible}>
+          <Row>
+            <Col1>
+              <h3>Height: {character.height}</h3>
+              <h3>Mass: {character.mass}</h3>
+              <h3>Hair Color: {character.hair_color}</h3>
+              <h3>Skin Color: {character.skin_color}</h3>
+              <h3>Eye Color: {character.eye_color}</h3>
+              <h3>Birth Year: {character.birth_year}</h3>
+              <h3>Gender: {character.gender}</h3>
+            </Col1>
+
+            <p>Films</p>
+          </Row>
+        </Col2>
+      </Row>
     </div>
   );
 };
